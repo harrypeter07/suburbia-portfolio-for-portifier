647ebda96e3ac72f659d768e91a986dedb5cc885
harrypeter07 <hassanmansuri570@gmail.com>
2025-03-04 09:03:30 +0530

    added animation

--- src/app/components/ParallaxImage.tsx
+++ src/app/components/ParallaxImage.tsx
@@ -1,5 +1,8 @@
+"use client"
+import clsx from 'clsx';
 import Image from 'next/image';
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRef } from 'react';
 
 type Props = {
     foregroundImage: string;
@@ -12,22 +15,66 @@ export  function ParallaxImage({
     backgroundImage,
     className
 }: Props) {
+
+    const backgroundImageRef = useRef<HTMLDivElement>(null);
+    const foregroundImageRef = useRef<HTMLDivElement>(null);
+
+    const targetPosition = useRef({ x: 0, y: 0 });
+    const currentPosition = useRef({ x: 0, y: 0 });
+    useEffect(()=>{
+        const frameId = requestAnimationFrame(animationFrame);
+    window.addEventListener('mousemove', onMouseMove);
+
+    function onMouseMove(event: MouseEvent){
+        const {innerWidth, innerHeight} = window;
+        const xPercent = (event.clientX / innerWidth - 0.5)*2  //rnage -1 to 1
+        const yPercent = (event.clientY / innerHeight - 0.5)*2  //rnage -1 to 1
+
+        // console.log(xPercent, yPercent)
+        targetPosition.current = {x: xPercent*-20, y: yPercent*-20}
+    }
+
+    function animationFrame (){
+        const {x: targetX , y:targetY} = targetPosition.current;
+        const {x: currentX, y: currentY} = currentPosition.current;
+
+        const newX = currentX + (targetX - currentX) * 0.1;
+        const newY = currentY + (targetY - currentY) * 0.1;
+        currentPosition.current = {x: newX, y: newY};
+
+        if(backgroundImageRef.current){
+            backgroundImageRef.current.style.transform = `translate(${newX}px, ${newY}px)`;
+        }
+        if(foregroundImageRef.current){
+            foregroundImageRef.current.style.transform = `translate(${newX * 3}px, ${newY * 3}px)`;
+        }
+        requestAnimationFrame(animationFrame);
+
+    }
+
+    return () => {
+        window.removeEventListener('mousemove', onMouseMove);
+        cancelAnimationFrame(frameId);
+    }
+
+    }, [])
+
   return (
-    <div>
+    <div className={clsx("grid grid-cols-1 place-items-center", className)}>
         
-        	<div className="backgroundImage">
+        	<div ref={backgroundImageRef} className="backgroundImage  col-start-1 row-start-1 transition-transform">
 					<Image
 						src={backgroundImage}
-						height={300}
-						width={300}
+						height={500}
+						width={500}
 						alt="bg-image"
 					/>
 				</div>
-                <div className="foreGroundImage">
+                <div ref={foregroundImageRef} className="foreGroundImage col-start-1 row-start-1 transition-transform h-full w-full place-items-center">
 					<Image
 						src={foregroundImage}
-						height={300}
-						width={300}
+						height={500}
+						width={500}
 						alt="bg-image"
 					/>
 				</div>
